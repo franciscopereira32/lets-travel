6e0d2baaa83028ded9f0dd98cec9194bff4c2d44
Francisco Pereira <80694995+franciscopereira32@users.noreply.github.com>
2024-02-09 18:42:23 +1030

    first routing

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/all', function(req, res) {
+  res.render('all_hotels', {title: 'All Hotels'});
+});
+
 module.exports = router;
