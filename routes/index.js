b4e5ef482f8fd184345008595d507f84fbd37256
Francisco Pereira <80694995+franciscopereira32@users.noreply.github.com>
2024-03-19 20:18:27 +1030

    saving images from the cloud

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,10 @@ router.get('/countries/:country', hotelController.hotelsByCountry);
 //ADMIN Routes
 router.get('/admin', hotelController.adminPage);
 router.get('/admin/add', hotelController.createHotelGet);
-router.post('/admin/add', hotelController.createHotelPost);
+router.post('/admin/add', 
+hotelController.upload,
+hotelController.pushToCloudinary,
+hotelController.createHotelPost);
 router.get('/admin/edit-remove', hotelController.editRemoveGet);
 router.post('/admin/edit-remove', hotelController.editRemovePost);
 router.get('/admin/:hotelId/update', hotelController.updateHotelGet);
