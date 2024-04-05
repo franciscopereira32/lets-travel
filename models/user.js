065cd240b9b963cf3c9dd089fec3054a8bd28fce
Francisco Pereira <80694995+franciscopereira32@users.noreply.github.com>
2024-04-05 20:53:59 +1030

    creating the user model

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongose');
+const { type } = require('os');
+
+const userSchema = new mongoose.userSchema({
+    first_name: {
+        type: String,
+        required: 'First name is required',
+        trim: true,
+        max: 30
+    },
+    surname: {
+        type: String,
+        required: "Surname is re required",
+        trim: true,
+        max: 30
+    },
+    email: {
+       type: String,
+       required: 'Email address is required',
+       trim: true,
+       unique: true,
+       lowercase: true 
+    },
+    password: {
+        type: String,
+        required: 'Password is required'
+    },
+    isAdmin: { 
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
